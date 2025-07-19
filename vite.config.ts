a784451cc5e9feec66e334f6ff82ee226472a721
ViniciusHashizume <viniciushashizume26@gmail.com>
2025-07-18 23:08:28 -0300

    Corrige caminhos para deploy final

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio2/',
+  base: '/Portfolio/', // DEVE USAR O NOME DO REPOSITÓRIO DO GITHUB
   plugins: [react()],
 })
