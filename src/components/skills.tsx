0e8a0f23394c2d7bcd2322b722b18a4295aff399
ViniciusHashizume <viniciushashizume26@gmail.com>
2025-07-31 15:38:38 -0300

    react ts upload

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -2,6 +2,8 @@ import { useLanguage } from '../contexts/languageContext';
 import { data } from '../data/portfolioData';
 
 const skillsBadges = [
+  "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+  "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
   "https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white",
   "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black",
   "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white",
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', // DEVE USAR O NOME DO REPOSITÓRIO DO GITHUB
+  base: '/Portfolio/', 
   plugins: [react()],
 })
